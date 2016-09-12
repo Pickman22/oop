80e8598863bfedc62bb6541a92105dafef0fcae4
pickman22 <jhestolano@gmail.com>
2016-09-11 19:20:21 -0500

    Removed crap

--- armgcc/CMakeFiles/parsed_uart.elf.dir/home/jorge/dev/C/embedded/projects/parsed_uart/reg.c.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/parsed_uart.elf.dir/home/jorge/dev/C/embedded/projects/parsed_uart/reg.c.obj: \
- /home/jorge/dev/C/embedded/projects/parsed_uart/reg.c \
- /home/jorge/dev/C/embedded/projects/parsed_uart/reg.h
-
-/home/jorge/dev/C/embedded/projects/parsed_uart/reg.h:
